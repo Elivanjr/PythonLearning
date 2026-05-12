1beb67c1409014fa1b248e236003209ef184b6bc
Elivan Junior <elivanvljr@gmail.com>
2026-05-12 16:44:37 -0300

    Delete fundamentals/conditionals/anoBisexto.py

--- fundamentals/conditionals/anoBisexto.py
+++
@@ -1,8 +0,0 @@
-year = int(input("Digite um ano qualquer ou o atual: "))
-
-if year % 4 == 0 and year % 100 != 0:
-    print(f"O ano de {year} é bissexto.")
-elif year % 200 == 0:
-    print(f"O ano {year} é bissexto.")
-else:
-    print(f"O ano {year} NÃO É BISSEXTO.")
